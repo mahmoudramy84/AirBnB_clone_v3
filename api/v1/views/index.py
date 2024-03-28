53a8176c6e5a7fdb32d0a262b6224da770dd7034
Mahmoud Elbehery <mahmoudramy84@gmail.com>
2024-03-28 09:51:32 +0200

    Adding 4. Some stats task

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,32 @@
 """ Index """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+
+hbnbText = {
+    "amenities": "Amenity",
+    "cities": "City",
+    "places": "Place",
+    "reviews": "Review",
+    "states": "State",
+    "users": "User"
+}
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of API """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def Stats():
+    """Stats"""
+    return_dict = {}
+    for key, value in hbnbText.items():
+        return_dict[key] = storage.count(value)
+    return jsonify(return_dict)
+
+if __name__ == "__main__":
+    pass
